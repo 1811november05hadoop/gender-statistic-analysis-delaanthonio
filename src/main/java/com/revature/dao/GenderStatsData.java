8a786a771575df82ebe39a7b13bc169da9bc25e1
Dela Anthonio <dell.anthonio@gmail.com>
2018-12-16 22:00:19 -0500

    [refactor] Clean up GenderStatsData

--- src/main/java/com/revature/dao/GenderStatsData.java
+++ src/main/java/com/revature/dao/GenderStatsData.java
@@ -5,22 +5,13 @@ import com.google.common.base.Splitter;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.hadoop.io.Text;
-import org.apache.log4j.Logger;
 
 public class GenderStatsData {
 
   public static final String FEMALE_GRADUATION_RATE_CODE = "SE.TER.CMPL.FE.ZS";
-  public static final String FEMALE_EMPLOYMENT_RATE_CODE = "SL.EMP.MPYR.FE.ZS";
+  public static final String FEMALE_EMPLOYMENT_RATE_CODE = "SL.EMP.TOTL.SP.FE.ZS";
   public static final String FEMALE_UPPER_SECONDARY_EDUCATION_RATE_CODE = "SE.SEC.CUAT.UP.FE.ZS";
-  public static final String MALE_EMPLOYMENT_RATE_CODE = "SL.EMP.MPYR.MA.ZS";
-
-  public static final int FIRST_YEAR = 1960;
-  public static final int LAST_YEAR = 2015;
-  private static final Logger LOGGER = Logger.getLogger(GenderStatsData.class);
-  private static final int COUNTRY_CODE_INDEX = 1;
-  private static final int INDICATOR_NAME_INDEX = 2;
-  private static final int INDICATOR_CODE_INDEX = 3;
-  private static final int FIRST_YEAR_INDEX = 4;
+  public static final String MALE_EMPLOYMENT_RATE_CODE = "SL.EMP.TOTL.SP.MA.ZS";
   private final List<String> data;
 
   public GenderStatsData(Text value) {
@@ -35,14 +26,15 @@ public class GenderStatsData {
       data.add(string);
     }
   }
+  
 
-  public String getData() {
-    return String.join(",", data);
-  }
+  public static class Key {
 
-
-  public String getIndicatorCode() {
-    return data.get(INDICATOR_CODE_INDEX);
+    public static final Text COUNTRY_NAME = new Text("CountryName");
+    public static final Text COUNTRY_CODE = new Text("CountryCode");
+    public static final Text INDICATOR_NAME = new Text("IndicatorName");
+    public static final Text INDICATOR_CODE = new Text("IndicatorCode");
+    public static final Text COLUMN_YEARS = new Text("ColumnYears");
   }
 
 
